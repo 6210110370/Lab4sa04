9caea265f741ace68612c78b4e8340b48316639a
6210110370 <6210110370@psu.ac.th>
2021-07-15 11:30:38 +0700

    30 minute : 2

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {ImageBackground, StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, ProgressBarAndroidComponent, StyleSheet, Text, View } from 'react-native';
 import Forecast from './Forcast';
 
 export default function Weather(props) {
@@ -12,17 +12,27 @@ export default function Weather(props) {
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <Text>Zip Code</Text>
-                <Text>{props.zipCode}</Text>
-                <Forecast {...forecastInfo} />
+                <View style={styles.infor}>
+                    <Text>Zip Code is {props.zipCode}</Text>
+                    <Forecast {...forecastInfo} />
+                </View>
             </ImageBackground>
         </View>
     );
 }
 const styles = StyleSheet.create({
     backdrop: {
-        alignItems: 'center',
+        flexDirection:'row',
+        alignItems: 'flex-start',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        
+    },
+    infor: {
+        flex:1,
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        alignItems: 'center',
+        color: 'white',
+        color: 'red'
     },
 });
