b60fcb3fbe226268af039324104745d8d8e1d691
6210110370 <6210110370@psu.ac.th>
2021-07-15 10:20:38 +0700

    add component

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text ,View } from 'react-native';
+import Weather from './components/Weather';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Weather zipCode="90110" />
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
